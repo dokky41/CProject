173dcf13d9a0de7c318858b1d5023bc28d86d9ec
Dokky <107966608+dokky41@users.noreply.github.com>
2023-04-13 22:53:16 +0900

    23/4/13(목) 서식지정자, 비트,비트연산

--- CProject/(2023.4.12)Variable.h
+++ CProject/(2023.4.12)Variable.h
@@ -41,7 +41,49 @@ void main()
 
 
 
+#pragma region 변수의 초기화
+	// 최초로 변수에 값을 저장해주는 과정입니다.
+	int exp = 100; // 변수의 선언
+	exp = 999;
+#pragma endregion
 
+#pragma region 상수
+	// 프로그램이 실행되는 동안 더 이상 값을 변경할 수 없는
+	// 메모리 공간입니다.
+		// 리터럴 값
+		// const float pi = 3.141592;
+		// L-value와 R-Value
+
+		// pi = 6.4571;	상수로 선언되었기 때문에 값을 변경할 수 없습니다.
+#pragma endregion
+
+#pragma region 산술 연산자
+	int result = (10 - 2) * 3;
+
+	int num1;
+	int num2;
+	int num3;
+	int num4;
+	int num5;
+
+	int B = 10;
+	int C = 5;
+	int E = 15;
+	const int F = 15;
+
+	// 1. num1 변수 = B 변수 + C 변수
+	num1 = B + C;
+	// 2. num2 변수 = E 변수 - F (심볼릭)상수
+	num2 = E - F;
+	// 3. num3 변수 = 리터럴 상수 * B 변수
+	num3 = 5 * B;
+	// 4. num4 변수 = 리터럴 상수 / F (심볼릭)상수
+	num4 = 15 / F;
+	// 5. num5 변수 = 리터럴 상수 + 리터럴 상수
+	num5 = 5 + 5;
+#pragma endregion
 
 
 }
+
+
