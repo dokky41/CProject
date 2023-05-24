d71ef12384beff85ffb59034e6d4b361bff546b4
Dokky <107966608+dokky41@users.noreply.github.com>
2023-05-24 21:47:32 +0900

    (2023.5.24) 텍스트 파일 가져오는 방법

---
+++ CProject/(2023.5.23)빌드과정.h
@@ -0,0 +1,138 @@
+#pragma once
+
+#pragma once
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <conio.h>
+#include <windows.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define LEFT 75
+#define RIGHT 77
+
+#define height 30
+#define width 60
+
+typedef struct Player
+{
+	//좌표
+	int x, y;
+	const char* shape;
+
+}Player;
+
+typedef struct Enemy
+{
+	//좌표
+	int x, y;
+	const char* shape;
+
+}Enemy;
+
+// 좌표 이동 함수
+void GotoXY(int x, int y)
+{
+	//좌표 정보를 저장하는 구조체 
+	COORD position = { x,y };
+
+	//좌표 정보를 설정
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), position);
+
+}
+
+void Keyboard(Player* ptrPlayer)
+{
+	char key = 0;
+
+	if (_kbhit())
+	{
+		key = _getch();
+
+		if (key == -32)
+		{
+			key = _getch();
+		}
+
+		switch (key)
+		{
+		case LEFT: if (ptrPlayer->x <= 0) return;  ptrPlayer->x -= 2;
+			break;
+		case RIGHT:if (ptrPlayer->x >= 28) return;  ptrPlayer->x += 2;
+			break;
+
+		}
+
+	}
+
+
+}
+
+int RandomX()
+{
+	srand(time(NULL));
+
+	int random = rand() % 31;
+
+	if (random % 2 == 1)
+	{
+		random += 1;
+	}
+
+	return random;
+}
+
+int main()
+{
+	system("mode con cols=30 lines=25");
+
+
+#pragma region 똥피하기 
+	// 1. x축으로만 이동
+	// 2. 콘솔 크기 (가로 : 30 세로 : 60)
+	// 3. x<0 작아졌다면 -로 이동못하도록
+	// 4. Enemy의 x축은 랜덤으로 설정되도록 하며, y축이 아래로 내려오도록 설정
+	// 똥과 플레이어가 충돌했다면 게임 오버
+	Player player = { 16,23,"★" };
+	Enemy enemy = { RandomX(),0,"♨" };
+
+	while (1)
+	{
+		Keyboard(&player);
+
+		GotoXY(enemy.x, enemy.y++);
+		printf("%s", enemy.shape);
+
+		if (enemy.y >= 24)
+		{
+			enemy.y = 0;
+			enemy.x = RandomX();
+		}
+
+		if (player.x == enemy.x && player.y == enemy.y)
+		{
+			printf("패배");
+			break;
+		}
+
+		GotoXY(player.x, player.y);
+		printf("%s", player.shape);
+
+		Sleep(100);
+		system("cls");
+
+	}
+
+
+
+#pragma endregion
+
+
+
+
+
+
+	return 0;
+}
--- CProject/source.cpp
+++ CProject/source.cpp
@@ -1,34 +1,12 @@
-#pragma once
-
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
 #include <conio.h>
 #include <windows.h>
-#include <stdlib.h>
-#include <time.h>
+#include "LoadManager.h"
 
-#define LEFT 75
-#define RIGHT 77
-
-#define height 30
-#define width 60
-
-typedef struct Player
-{
-	//좌표
-	int x, y;
-	const char* shape;
-
-}Player;
-
-typedef struct Enemy
-{
-	//좌표
-	int x, y;
-	const char* shape;
-
-}Enemy;
+#define UP 72
+#define DOWN 80
 
 // 좌표 이동 함수
 void GotoXY(int x, int y)
@@ -41,7 +19,15 @@ void GotoXY(int x, int y)
 
 }
 
-void Keyboard(Player* ptrPlayer)
+typedef struct Select
+{
+	int x, y;
+	const char* shape;
+
+}Select;
+
+
+void Keyboard(Select * selectPtr)
 {
 	char key = 0;
 
@@ -56,79 +42,62 @@ void Keyboard(Player* ptrPlayer)
 
 		switch (key)
 		{
-		case LEFT: if (ptrPlayer->x <= 0) return;  ptrPlayer->x -= 2;
+		case UP: selectPtr->y -= 5;
 			break;
-		case RIGHT:if (ptrPlayer->x >= 28) return;  ptrPlayer->x += 2;
+		case DOWN: selectPtr->y += 5;
 			break;
 
 		}
 
+	
 	}
 
 
 }
 
-int RandomX()
+//타이핑 효과
+void Typing(unsigned int speed, const char * content)
 {
-	srand(time(NULL));
+	int i = 0;
 
-	int random = rand() % 31;
-
-	if (random % 2 == 1)
+	while (content[i] != '\0')
 	{
-		random += 1;
+		printf("%c", content[i++]);
+		fflush(stdout); //버퍼 비우기
+		Sleep(speed);
 	}
 
-	return random;
 }
+
+
 
 int main()
 {
-	system("mode con cols=30 lines=25");
+	
+	//ReadTextFile("Resources/DB.txt");
+	//Typing(100, "Hello~");
+	
+	int stage = 0;
 
-
-#pragma region 똥피하기 
-	// 1. x축으로만 이동
-	// 2. 콘솔 크기 (가로 : 30 세로 : 60)
-	// 3. x<0 작아졌다면 -로 이동못하도록
-	// 4. Enemy의 x축은 랜덤으로 설정되도록 하며, y축이 아래로 내려오도록 설정
-	// 똥과 플레이어가 충돌했다면 게임 오버
-	Player player = { 16,23,"★" };
-	Enemy enemy = { RandomX(),0,"♨" };
+	Select select = { 15,29,"☞" };
 
 	while (1)
 	{
-		Keyboard(&player);
 
-		GotoXY(enemy.x, enemy.y++);
-		printf("%s", enemy.shape);
+		Keyboard(&select);
 
-		if (enemy.y >= 24)
+		switch (stage)
 		{
-			enemy.y = 0;
-			enemy.x = RandomX();
+			case 0 : ReadTextFile("Resources/DB.txt");
+				break;
+			case 1 : ReadTextFile("Resources/DB.txt");
+				break;
 		}
-
-		if (player.x == enemy.x && player.y == enemy.y)
-		{
-			printf("패배");
-			break;
-		}
-
-		GotoXY(player.x, player.y);
-		printf("%s", player.shape);
-
-		Sleep(100);
+		
+		GotoXY(select.x,select.y);
+		printf("%s", select.shape);
 		system("cls");
-
 	}
-
-
-
-#pragma endregion
-
-
-
 
 
 
